22e12ade4b4c2eb59bcb4a7507358e812ec0b157
Kat Berge <kat.n.berge@gmail.com>
2020-07-15 17:06:12 -0500

    adds circle contructor and a starting circle

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ document.addEventListener("DOMContentLoaded", () => {
     // set height of canavs to 400px
     canvas.height = 400;
 
+    // add a circle constructor
+    function Circle(x, y) {
+        this.x = x;
+        this.y = y;
+        this.r = 5; // radius
+        this.draw = () => {
+            c.beginPath();
+            c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+            c.stroke();
+            c.fill();
+        };
+    };
+
+    // draw new circle in center of canvas
+    let circle = new Circle(canvas.width / 2, canvas.height / 2);
+    circle.draw();
+
 });

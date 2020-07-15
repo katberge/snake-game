a7e61b8650db2fad117a141abb1aafdb9488e6b1
Kat Berge <kat.n.berge@gmail.com>
2020-07-15 16:17:11 -0500

    adds initial canvas height and width

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const canvas = document.querySelector("canvas");
     const c = canvas.getContext("2d");
 
+    // set width of canvas to 300px
+    canvas.width = 300;
+    // set height of canavs to 400px
+    canvas.height = 400;
+
 });

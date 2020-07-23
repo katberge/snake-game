7c2feac15fd8417bb158ab527416f9a9e64bf4a4
Kat Berge <kat.n.berge@gmail.com>
2020-07-23 12:27:48 -0500

    adds rainbow theme to color theme options

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const bopColors = ["#f969b2", "#e66e70", "#ef8429", "#dcc147", "#1cba6f", "#21d0aa", "#16498a", "#2f2489", "#2a374a", "#933153", "#b11620"];
     const ccColors = ["#ffafb8", "#ffafe0", "#f6afff", "#d0a4ff", "#c6b5ff", "#c3c7ff", "#c3e5ff", "#c3fffb"];
     const cottageColors = ["#ffc1b1", "#ffe8b1", "#c4f0d2", "#b1efff", "#d7def2", "#9bc99e", "#6b90b6", "#c99bc6", "#b6916b", "#b66c6b"];
+    const rainbowColors = ["#ff0000", "#ff8000", "#f2f207", "#00ff00", "#00ffbf", "#00ffff", "#0080ff", "#0000ff", "#8000ff", "#ff00ff"];
     let colors;
 
     // sets radius
@@ -210,6 +211,8 @@ document.addEventListener("DOMContentLoaded", () => {
                     colors = ccColors;
                 } else if (e.target.id === "cottage") {
                     colors = cottageColors;
+                } else if (e.target.id === "rainbow") {
+                    colors = rainbowColors;
                 }
                 e.target.classList.add("selected");
                 drawPreview();

4e32ad1d56eec4ffa31ccd4f84caa9e1c84be8c8
Kat Berge <kat.n.berge@gmail.com>
2020-07-23 10:26:16 -0500

    changes a color in the cottage palette to be more visable

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // color palette
     const bopColors = ["#f969b2", "#dcc147", "#1cba6f", "#21d0aa", "#16498a", "#2f2489", "#ef8429", "#e66e70", "#b11620", "#933153", "#2a374a"];
     const ccColors = ["#ffc3f3", "#f6afff", "#d0a4ff", "#c6b5ff", "	#c3c7ff", "#c3e5ff"];
-    const cottageColors = ["#c4f0e8", "	#ffc1b1", "#f2ebd7", "#9bc99e", "#b6916b", "#d7def2", "#b66c6b", "#6b90b6", "#b1efff", "#c99bc6"];
+    const cottageColors = ["#c4f0e8", "	#ffc1b1", "#ffe8b1", "#9bc99e", "#b6916b", "#d7def2", "#b66c6b", "#6b90b6", "#b1efff", "#c99bc6"];
     let colors = bopColors;
 
     // sets radius
